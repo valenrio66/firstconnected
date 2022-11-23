3b87b4cf0bc28cea0347389183c498c88f6421b2
valenrio66 <vrionald@gmail.com>
2022-11-23 19:04:55 +0700

    Connected to MongoDB

--- configs/env.go
+++ configs/env.go
@@ -10,7 +10,7 @@ import (
 func EnvMongoURI() string {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error on loading .env file")
 	}
 
 	return os.Getenv("MONGOURI")
