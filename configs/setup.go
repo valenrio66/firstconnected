fdc9f2df6ba4380658c228f5fd590e44d4f90095
Valen Rionald <vrionald@gmail.com>
2022-12-16 08:20:34 +0700

    Update JWT Again

--- configs/setup.go
+++ configs/setup.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"time"
 
-	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -39,34 +38,4 @@ var DB *mongo.Client = ConnectDB()
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	collection := client.Database("dbmuseum").Collection(collectionName)
 	return collection
-}
-
-type Config struct {
-	DBUri                  string        `mapstructure:"MONGODB_LOCAL_URI"`
-	RedisUri               string        `mapstructure:"REDIS_URL"`
-	Port                   string        `mapstructure:"PORT"`
-	AccessTokenPrivateKey  string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY"`
-	AccessTokenPublicKey   string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY"`
-	RefreshTokenPrivateKey string        `mapstructure:"REFRESH_TOKEN_PRIVATE_KEY"`
-	RefreshTokenPublicKey  string        `mapstructure:"REFRESH_TOKEN_PUBLIC_KEY"`
-	AccessTokenExpiresIn   time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRED_IN"`
-	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRED_IN"`
-	AccessTokenMaxAge      int           `mapstructure:"ACCESS_TOKEN_MAXAGE"`
-	RefreshTokenMaxAge     int           `mapstructure:"REFRESH_TOKEN_MAXAGE"`
-}
-
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigType("env")
-	viper.SetConfigName("app")
-
-	viper.AutomaticEnv()
-
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
-	}
-
-	err = viper.Unmarshal(&config)
-	return
 }
--- main.go
+++ main.go
@@ -1,52 +1,28 @@
 package main
 
 import (
-	"context"
+	"gin-mongo-api/configs"
+	"gin-mongo-api/routes" //add this
 	"os"
 
-	"gin-mongo-api/configs"
-	"gin-mongo-api/controllers"
-	"gin-mongo-api/routes"
-	"gin-mongo-api/services"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-var (
-	server      *gin.Engine
-	ctx         context.Context
-	mongoclient *mongo.Client
-
-	userService         services.UserService
-	UserController      controllers.UserController
-	UserRouteController routes.UserRouteController
-
-	authCollection      *mongo.Collection
-	authService         services.AuthService
-	AuthController      controllers.AuthController
-	AuthRouteController routes.AuthRouteController
 )
 
 func main() {
 	router := gin.Default()
 
+	//run database
 	configs.ConnectDB()
 
-	userService = services.NewUserServiceImpl(authCollection, ctx)
-	authService = services.NewAuthService(authCollection, ctx)
-	AuthController = controllers.NewAuthController(authService, userService)
-	AuthRouteController = routes.NewAuthRouteController(AuthController)
+	//routes
+	routes.InvertebrataRoute(router)      //add this
+	routes.VertebrataRoute(router)        //add this
+	routes.FosilRoute(router)             //add this
+	routes.BatuanRoute(router)            //add this
+	routes.SumberDayaGeologiRoute(router) //add this
+	routes.LokasiTemuanRoute(router)      //add this
+	routes.KoordinatRoute(router)         //add this
 
-	UserController = controllers.NewUserController(userService)
-	UserRouteController = routes.NewRouteUserController(UserController)
-
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{"https://localhost:8888", "http://localhost:3000"}
-	corsConfig.AllowCredentials = true
-
-	server.Use(cors.New(corsConfig))
 	router.Run(":" + SetPort())
 }
 
