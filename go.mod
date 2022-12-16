aee841f3bfc2bc952170b1a3c3f71d78f50b1384
Valen Rionald <vrionald@gmail.com>
2022-12-17 01:05:17 +0700

    JWT Try Again

--- go.mod
+++ go.mod
@@ -41,6 +41,8 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/cors v1.8.1 // indirect
+	github.com/rs/cors/wrapper/gin v0.0.0-20221003140808-fcebdb403f4d // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
--- main.go
+++ main.go
@@ -2,41 +2,62 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+
 	"gin-mongo-api/configs"
 	"gin-mongo-api/controllers"
-	"gin-mongo-api/routes" //add this
+	"gin-mongo-api/routes"
 	"gin-mongo-api/services"
-	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
-	ctx                 context.Context
+	server      *gin.Engine
+	ctx         context.Context
+	mongoclient *mongo.Client
+
 	userService         services.UserService
 	UserController      controllers.UserController
 	UserRouteController routes.UserRouteController
+
 	authCollection      *mongo.Collection
 	authService         services.AuthService
 	AuthController      controllers.AuthController
 	AuthRouteController routes.AuthRouteController
 )
 
-func main() {
-	router := gin.Default()
+func init() {
+	configs, err := configs.LoadConfig(".")
+	_ = configs
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
 
-	//run database
-	configs.ConnectDB()
+	ctx = context.TODO()
 
-	//routes
-	routes.InvertebrataRoute(router)      //add this
-	routes.VertebrataRoute(router)        //add this
-	routes.FosilRoute(router)             //add this
-	routes.BatuanRoute(router)            //add this
-	routes.SumberDayaGeologiRoute(router) //add this
-	routes.LokasiTemuanRoute(router)      //add this
-	routes.KoordinatRoute(router)         //add this
+	// Connect to MongoDB
+	mongoconn := options.Client().ApplyURI("mongodb+srv://valenrionald:06082002@cluster0.c9pmzz0.mongodb.net/?retryWrites=true&w=majority")
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := mongoclient.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("MongoDB successfully connected...")
+
+	// Collections
+	authCollection = mongoclient.Database("dbmuseum").Collection("user")
 	userService = services.NewUserServiceImpl(authCollection, ctx)
 	authService = services.NewAuthService(authCollection, ctx)
 	AuthController = controllers.NewAuthController(authService, userService)
@@ -45,13 +66,39 @@ func main() {
 	UserController = controllers.NewUserController(userService)
 	UserRouteController = routes.NewRouteUserController(UserController)
 
-	router.Run(":" + SetPort())
+	server = gin.Default()
 }
 
-func SetPort() string {
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "80"
+func main() {
+	configs, err := configs.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Could not load config", err)
 	}
-	return port
+
+	defer mongoclient.Disconnect(ctx)
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"https://localhost:8888", "http://localhost:3000"}
+	corsConfig.AllowCredentials = true
+
+	server.Use(cors.New(corsConfig))
+
+	// router := gin.Default()
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success"})
+	})
+
+	AuthRouteController.AuthRoute(router, userService)
+	UserRouteController.UserRoute(router, userService)
+	log.Fatal(server.Run(":" + configs.Port))
+
+	// routes.InvertebrataRoute(router)      //add this
+	// routes.VertebrataRoute(router)        //add this
+	// routes.FosilRoute(router)             //add this
+	// routes.BatuanRoute(router)            //add this
+	// routes.SumberDayaGeologiRoute(router) //add this
+	// routes.LokasiTemuanRoute(router)      //add this
+	// routes.KoordinatRoute(router)         //add this
 }
